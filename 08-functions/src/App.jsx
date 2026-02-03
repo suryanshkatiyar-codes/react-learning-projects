ef676b715c2740d3d284160f16720f9cf95f2d3a
Suryansh Katiyar <2k23.aids2310309@gmail.com>
2026-02-03 12:11:55 +0530

    Functions Completed

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const App = () => {
+
+  function pageScrolling(val){
+    if(val>0){
+      console.log("Sidha Scrolling");
+    }
+    else{
+      console.log("Ulta Scrolling");
+    }
+  }
+
   return (
-    <div>
-      App
+    <div onWheel={(elem)=>{
+      pageScrolling(elem.deltaY);
+    }} className='parent'>
+      <div className='top'></div>
+      <div className='middle'></div>
+      <div className='bottom'></div>
     </div>
   )
 }
