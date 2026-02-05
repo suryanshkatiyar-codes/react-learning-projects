6b15169e57fb70edc302a254c46eda45b634d506
Suryansh Katiyar <2k23.aids2310309@gmail.com>
2026-02-06 00:36:42 +0530

    Add 05-css project

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>
+      we are in button
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.btn}>
+      we are in header
+    </div>
+  )
+}
+
+export default Header
